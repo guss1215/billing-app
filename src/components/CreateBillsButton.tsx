11a583bace5beab3d9d340f5fe464d029549a341
Gustavo Guillen <guss1215@gmail.com>
2023-09-08 15:25:24 -0400

    not duplicated bills creation control

--- src/components/CreateBillsButton.tsx
+++ src/components/CreateBillsButton.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { toast } from 'react-toastify'; // Import toast from react-toastify
+import api from '../services/api';
 import './CreateBillsButton.css';
 
 interface CreateBillsButtonProps {
@@ -10,16 +11,28 @@ const CreateBillsButton: React.FC<CreateBillsButtonProps> = ({ onCreateBills }) 
     const [period, setPeriod] = useState('');
     const [category, setCategory] = useState('');
 
-    const handleCreateBills = () => {
-        onCreateBills(period, category);
-        setPeriod('');
-        setCategory('');
+    const handleCreateBills = async () => {
+        const existingBill = await checkBillExists(period, category);
 
-        // Trigger a toast notification for bill creation
-        toast.success('Bills created successfully!', {
-            autoClose: 3000,
-            position: 'top-right',
-        });
+        if (existingBill) {
+            // Display a user-friendly message to the user
+            // You can use a notification library or a modal to display this message
+            // Example using the 'react-toastify' library:
+            toast.error('Bill already exists for selected period and category.', {
+                autoClose: 3000, // Close the toast after 3 seconds
+                position: 'top-right', // Position the toast at the top-right corner
+            });
+        } else {
+            onCreateBills(period, category);
+            setPeriod('');
+            setCategory('');
+
+            // Trigger a toast notification for bill creation
+            toast.success('Bills created successfully!', {
+                autoClose: 3000,
+                position: 'top-right',
+            });
+        }
     };
 
     const handlePeriodChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -29,6 +42,17 @@ const CreateBillsButton: React.FC<CreateBillsButtonProps> = ({ onCreateBills }) 
             setPeriod(value);
         }
     };
+
+    const checkBillExists = async (period: string, category: string) => {
+        try {
+            const response = await api.get(`/check-bill-exists?period=${period}&category=${category}`);
+            return response.data; // Returns true if the bill exists, false otherwise
+        } catch (error) {
+            console.error('Error checking bill existence:', error);
+            return false; // Return false on error or if the bill doesn't exist
+        }
+    };
+
 
     return (
         <div className='create-bills-button'>
